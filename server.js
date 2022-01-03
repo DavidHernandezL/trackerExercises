a7932488121017042e844bfa7649fffe779f8b04
David <HL_David@outlook.com>
2022-01-03 11:09:19 -0600

    server config

--- server.js
+++ server.js
@@ -1,17 +1,22 @@
-const express = require('express')
-const app = express()
-const cors = require('cors')
-require('dotenv').config()
+const express = require('express');
+const cors = require('cors');
+require('dotenv').config();
+const mongoose = require('mongoose');
+const routes = require('./routes/index');
 
+//Database
+require('./database');
+
+//Config
+const app = express();
+//Middleware  
+app.use(express.urlencoded({extended:false}));
+app.use(express.json());
 app.use(cors())
 app.use(express.static('public'))
-app.get('/', (req, res) => {
-  res.sendFile(__dirname + '/views/index.html')
-});
 
-
-
-
+//Routes
+routes(app);
 
 const listener = app.listen(process.env.PORT || 3000, () => {
   console.log('Your app is listening on port ' + listener.address().port)
